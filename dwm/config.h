3a53edbc2633d2ac137fd92ab2fa2f52e7129c1e
oimickboi <oimickboi@gmail.com>
2022-12-23 12:49:09 -0700

    edited status bar and shortcuts

--- dwm/config.h
+++ dwm/config.h
@@ -55,6 +55,7 @@ static const Rule rules[] = {
 	{ "Gimp",       NULL,       NULL,       0,            1,           -1 },
 	{ "Surf",       NULL,       NULL,       1 << 1,       0,           -1 },
 	{ "qutebrowser",NULL,       NULL,       1 << 1,       0,           -1 },
+	{ "firefox-bin",NULL,       NULL,       1 << 1,       0,           -1 },
 };
 
 /* layout(s) */
@@ -85,17 +86,20 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
-static const char *webcmd[]   = { "qutebrowser", NULL };
+static const char *webcmd[]   = { "firefox-bin", NULL };
+//static const char *webcmd[]   = { "qutebrowser", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
-	{ MODKEY|ShiftMask,                       XK_u,      spawn,          SHCMD("emacsclient -a '' -c") },
-	{ MODKEY|ShiftMask,             XK_o,      spawn,          SHCMD("tabbed -r2 -c surf -e x search.brave.com & disown") },
-	{ MODKEY,                       XK_o,      spawn,          {.v = webcmd } },
+	{ MODKEY|ShiftMask,             XK_e,      spawn,          SHCMD("emacsclient -a '' -c") },
+	{ MODKEY,                       XK_r,      spawn,          {.v = webcmd } },
+	//{ MODKEY|ShiftMask,             XK_r,      spawn,          SHCMD("tabbed -r2 -c surf -e x search.brave.com & disown") },
+	//{ MODKEY|ShiftMask,             XK_o,      spawn,          SHCMD("tabbed -r2 -c surf -e x search.brave.com & disown") },
+	//{ MODKEY,                       XK_o,      spawn,          {.v = webcmd } },
 	{ MODKEY,                       XK_u,      spawn,          SHCMD("st ranger") },
 	{ MODKEY|ShiftMask,             XK_p,      spawn,          SHCMD("st mocp") },
-	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
@@ -103,7 +107,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-	{ MODKEY,                       XK_Return, zoom,           {0} },
+	{ MODKEY|ShiftMask,             XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
